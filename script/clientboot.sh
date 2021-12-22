dffbdb951c9b860b2f124b73008f84b634b72ba1
chihkuo <chihkuo@gmail.com>
2021-12-22 15:55:24 +0800

    modify client boot

--- script/clientboot.sh
+++ script/clientboot.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-touch /home/linaro/clientboot.txt
+#touch /home/linaro/clientboot.txt
 
-sleep 10
-sudo killall wpa_supplicant
-sleep 1
-sudo /sbin/wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf
+#sleep 10
+#sudo killall wpa_supplicant
+#sleep 1
+#sudo /sbin/wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf
+
+if [[ -e '/home/linaro/apboot.txt' ]]; then
+	echo "check network"
+	sudo ping www.google.com -w 5
+	if [[ $? == 0 ]]; then
+		echo "network ok, remove apboot.txt"
+		rm /home/linaro/apboot.txt
+	else
+		echo "network fail, return AP mode"
+		/home/linaro/init/setmode.sh ap
+	fi
+fi
