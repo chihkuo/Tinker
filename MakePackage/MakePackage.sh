4cc7980c43162f00933fa854c49a8b4978f41d3d
chihkuo <mooijkuo@gmail.com>
2022-10-12 14:05:05 +0800

    add Mackage script

---
+++ MakePackage/MakePackage.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+function copy() {
+	echo "run function copy()"
+	if [ ! -d "update" ]; then
+		mkdir update
+	fi
+	cp -f ../datalog-h5000/dlg320.exe ./update/
+	cp -f ../DataProgram/DataProgram.exe ./update/
+	cp -f ../dlsocket/DLsocket.exe ./update/
+	cp -f ../SWupdate/SWupdate.exe ./update/
+#	cp -f ../FWupdate/FWupdate.exe ./update/
+	cp -f ../script/dlsetting ./update/
+	cp -f ../script/newSWupdate.sh ./update/
+	cp -f ../script/update.sh ./update/
+	sync
+}
+
+function package() {
+	echo "run function package()"
+	tar -cvf update_$1.tar update
+	sync
+}
+
+function clean() {
+	echo "run function clean()"
+	rm -rf update
+	sync
+}
+
+function all() {
+	echo "run function all()"
+	copy;
+	package $1;
+	clean;
+}
+
+if [ $1 == "copy" ]; then
+	copy;
+fi
+
+if [ $1 == "package" ]; then
+	package $2;
+fi
+
+if [ $1 == "clean" ]; then
+	clean;
+fi
+
+if [ $1 == "all" ]; then
+	all $2;
+fi
