e812dffe7a46a13ae6b9b92272b3cd44b83c2bea
chihkuo <mooijkuo@gmail.com>
2022-12-22 15:47:55 +0800

    build & run OK, update to V2.8.6

--- DataProgram/main.c
+++ DataProgram/main.c
@@ -14,7 +14,7 @@
 #include "../common/base64.h"
 #include "../common/SaveLog.h"
 
-#define VERSION         "2.8.5"
+#define VERSION         "2.8.6"
 //#define USB_PATH        "/tmp/usb"
 //#define USB_PATH        "/tmp/run/mountd/sda1"
 //#define USB_PATH        "/mnt"
